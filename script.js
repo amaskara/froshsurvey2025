87a3abf55ae6c747dac99387594cf79c8b3df916
Anika Maskara <66639650+amaskara@users.noreply.github.com>
2021-09-13 03:44:20 +0000

    demograpics

---
+++ script.js
@@ -0,0 +1,14 @@
+function expand() {
+  const topNav = document.getElementById("top-nav");
+  if (topNav.className.includes("expand")) {
+    topNav.classList.remove("expand");
+  } else topNav.classList.add("expand");
+}
+
+
+AOS.init({
+  duration: 1200,
+})
+
+
+
